63769bdfe062120bc70e851fe3eff988934901b4
TamGh <49073475+TamGh@users.noreply.github.com>
2021-03-27 05:45:06 +0400

    Create initial project and enable login process

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public authService: AuthService)
+    { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm(){
+    this.form = this.formBuilder.group(
+      {
+        username: ['testUser@yopmail.com'],
+        password: ['test!12345']
+      }
+    );
+  }
+
+  login() {
+    this.authService.login(this.form.get('username').value, this.form.get('password').value)
+      .subscribe(res => {
+        localStorage.setItem('access_token', res.data);
+        localStorage.setItem('userName', this.form.get('username').value);
+
+        this.authService.TokenSubject.next('Bearer ' + res.data);
+      })
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('userName') != null;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  url = environment.apiUrl;
+
+  TokenSubject = new BehaviorSubject<string>(null);
+
+
+  constructor(private http:HttpClient) { }
+
+  login(userName: string, password: string): Observable<any> {
+      return this.http.post(`${this.url}api/accounts/login`, {userName, password});
+  }
+
+  logout(){
+    localStorage.clear();
+    this.TokenSubject.next(null);
+  }
+}
