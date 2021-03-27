6b162ad092422257ca01b9dddf8e005179b4fa5a
TamGh <49073475+TamGh@users.noreply.github.com>
2021-03-27 07:04:40 +0400

    Product list with delete functionality

---
+++ src/app/models/product.interfaces.ts
@@ -0,0 +1,22 @@
+export interface IProductListItem {
+    id: number;
+    name: string;
+    available: boolean;
+    price: number;
+}
+
+export interface IProduct {
+    id: number;
+    name: string;
+    available: boolean;
+    price: number;
+    description: string;
+    dateCreated: Date;
+}
+
+export interface IResponseModel<T> {
+    data: T,
+    message? : string;
+    responseType?: number;
+}
+
